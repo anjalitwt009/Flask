aa57d8b625cc39c9d1a54294f01cdbbaffd038b8
Anjali Thawait <64317446+anjalitwt009@users.noreply.github.com>
2020-06-14 11:33:02 +0530

    Update routes.py

--- Assignment/Exp_code/routes.py
+++ Assignment/Exp_code/routes.py
@@ -2,11 +2,12 @@ from flask import render_template, url_for, flash, redirect, request, abort,json
 from Exp_code import app
 from werkzeug.utils import secure_filename
 import pandas as pd
+import numpy as np
 import csv
 import os
 from datetime import datetime
 import json, json2html
-UPLOAD_FOLDER = r'C:\Users\user\PycharmProjects\New folder'
+UPLOAD_FOLDER = r'C:\Users\user\PycharmProjects\Assignment'
 ALLOWED_EXTENSIONS = set(['csv'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -34,10 +35,10 @@ def upload():
 @app.route("/analyze")
 def analyze():
     upload_time = datetime.utcnow()
-    for files in os.listdir(r"C:\Users\user\PycharmProjects\New folder"):
+    for files in os.listdir(r"C:\Users\user\PycharmProjects\Assignment"):
         if files.endswith(".csv"):
-            df = pd.read_csv(r"C:\Users\user\PycharmProjects\New folder\data.csv")
-            lookup_df = pd.read_csv(r"C:\Users\user\PycharmProjects\New folder\lookup.csv")
+            df = pd.read_csv(r"C:\Users\user\PycharmProjects\Assignment\data.csv")
+            lookup_df = pd.read_csv(r"C:\Users\user\PycharmProjects\Assignment\lookup.csv")
 
             rows_before= len(df.index)
 
@@ -49,7 +50,7 @@ def analyze():
             data_df_new = data_df_new.rename({'Sale Type': 'SaleType_Clean'}, axis=1)
             data_df_new = pd.DataFrame(data_df_new)
 
-            data_df_new.to_csv(r"C:\Users\user\PycharmProjects\New folder\SalesFile.csv")
+            data_df_new.to_csv(r"C:\Users\user\PycharmProjects\Assignment\SalesFile.csv")
 
             earliest_date = data_df_new['AccountingDate'].min()
             recent_date = data_df_new['AccountingDate'].max()
@@ -69,12 +70,14 @@ def analyze():
             data_df3.sort_values(by='Date', ascending=True, inplace=True)
 
             data_pt=[]
-            df1 = data_df_new.loc['2017-7-1':'2017-7-31', data_df_new.columns.get_loc('DealNo')]
-            df2 = data_df_new.loc['2017-8-1':'2017-8-31', data_df_new.columns.get_loc('DealNo')]
-            df3 = data_df_new.loc['2017-9-1':'2017-9-30', data_df_new.columns.get_loc('DealNo')]
-            df4 = data_df_new.loc['2017-10-1':'2017-10-31', data_df_new.columns.get_loc('DealNo')]
-            df5 = data_df_new.loc['2017-11-1':'2017-11-30', data_df_new.columns.get_loc('DealNo')]
-            df6 = data_df_new.loc['2017-12-1':'2017-7-31', data_df_new.columns.get_loc('DealNo')]
+
+            df1 = data_df_new.loc['2017-6-1':'2017-6-30', ['DealNo', 'DealType']]
+            df2 = data_df_new.loc['2017-7-1':'2017-7-31', ['DealNo','DealType']]
+            df3 = data_df_new.loc['2017-8-1':'2017-8-31', ['DealNo','DealType']]
+            df4 = data_df_new.loc['2017-9-1':'2017-9-30', ['DealNo','DealType']]
+            df5 = data_df_new.loc['2017-10-1':'2017-10-31', ['DealNo','DealType']]
+            df6 = data_df_new.loc['2017-11-1':'2017-11-30', ['DealNo','DealType']]
+
 
             dp1= df1['DealNo'].max()-df1['DealNo'].min()
             data_pt.append(dp1)
@@ -89,8 +92,23 @@ def analyze():
             dp6 = df6['DealNo'].max() - df6['DealNo'].min()
             data_pt.append(dp6)
 
+            counts1 = df1['DealType'].value_counts().tolist()
+            counts2 = df2['DealType'].value_counts().tolist()
+            counts3 = df3['DealType'].value_counts().tolist()
+            counts4 = df4['DealType'].value_counts().tolist()
+            counts5 = df5['DealType'].value_counts().tolist()
+            counts6 = df6['DealType'].value_counts().tolist()
+            values = df6['DealType'].value_counts().keys().tolist()
+
+            #data_values = np.array([counts1,counts2,counts3,counts4,counts5,counts6])
+            #dataset_values = pd.DataFrame(data=data_values, index=["June", "July","August","September",
+                                                                   #"October","November"],
+                                         # columns=["column1", "column2","column3", "column4"])
+            #dataset_values=dataset_values.transpose()
+
             return render_template('report.html', upload_time=upload_time,earliest_date=earliest_date,
                                    recent_date=recent_date,number_of_rows_removed=number_of_rows_removed,
-                                   tables=[stats.to_html()],data=to_json(data_pt))
+                                   tables=stats,counts1=(counts1),counts2=counts2,counts3=counts3,counts4=counts4,
+                                   counts5=counts5,counts6=counts6,values=values)
 
 
